f706e04757eb81a80650cdefbb86a63ef57df0f4
TeamInnexia <81477304+TeamInnexia@users.noreply.github.com>
2021-05-14 01:29:32 +0530

    Update repo.py

--- wbb/modules/repo.py
+++ wbb/modules/repo.py
@@ -28,14 +28,14 @@ from wbb.core.decorators.errors import capture_err
 from wbb.utils.fetch import fetch
 
 __MODULE__ = "Repo"
-__HELP__ = "/repo - To Get My Github Repository Link " "And Support Group Link"
+__HELP__ = "/repo - To Get My GitHub And Support Group Link"
 
 
 @app.on_message(filters.command("repo") & ~filters.edited)
 @capture_err
 async def repo(_, message):
     users = await fetch(
-        "https://api.github.com/repos/thehamkercat/williambutcherbot/contributors"
+        "https://api.github.com/repos/TeamInnexia/innexiabot/contributors"
     )
     list_of_users = ""
     count = 1
@@ -43,7 +43,7 @@ async def repo(_, message):
         list_of_users += f"**{count}.** [{user['login']}]({user['html_url']})\n"
         count += 1
 
-    text = f"""[Github](https://github.com/thehamkercat/WilliamButcherBot) | [Group](t.me/PatheticProgrammers)
+    text = f"""[Github](https://github.com/TeamInnexia/innexiabot) | [Group](t.me/Rexomachat)
 ```----------------
 | Contributors |
 ----------------```

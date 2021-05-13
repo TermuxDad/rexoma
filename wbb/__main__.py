968f57c7f6d22974112dd2c7d544cd0aec22d9d4
TermuxDad <76565235+TermuxDad@users.noreply.github.com>
2021-05-14 02:15:30 +0530

    Update __main__.py

--- wbb/__main__.py
+++ wbb/__main__.py
@@ -103,6 +103,7 @@ async def help_command(_, message):
                 InlineKeyboardButton(
                     text="Summon Me",
                     url=f"http://t.me/{BOT_USERNAME}?startgroup=new",
+                ),
             ],
             [
                 InlineKeyboardButton(

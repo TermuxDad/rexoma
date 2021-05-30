8b1545b20b554778d69d5d5df37317360d65154e
TermuxDad <76565235+TermuxDad@users.noreply.github.com>
2021-05-30 06:40:29 +0530

    Create vcplayer.py

---
+++ wbb/modules/vcplayer.py
@@ -0,0 +1,24 @@
+__mod_name__ = "Vc Player"
+
+__help__ = """
+*START UP*
+1 Make Bot Admin 
+2 Add @Rexoma_Music To Your Group
+3 Start A Voice Chat And Give Commands
+
+**Commands ⚒️**
+
+/play - reply to youtube url or song file to play song
+/play <song name> - play song you requested
+/song <song name> - download songs you want quickly
+/search <query> - search videos on youtube with details
+
+**Admins Command 👮‍♂**
+
+/pause - pause song play
+/resume - resume song play
+/skip - play next song
+/end - stop music play
+
+**PLEASE NOTE THIS SERVICE IS UNSTABLE AND CAN BE STOPPED ANYTIME** 
+"""
